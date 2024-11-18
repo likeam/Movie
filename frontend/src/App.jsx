f3b2e5b491cdc674284d54d6cd45f5679c7ee347
abdul <likeam99@gmail.com>
2024-11-18 10:21:59 +0500

    Building User Auth section on frontend sid

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
+  );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,40 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+import PrivateRooute from "./pages/Auth/PrivateRooute.jsx";
+import Profile from "./pages/User/Profile.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+//     Auth //
+
+// Restricted
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="" element={<PrivateRooute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
